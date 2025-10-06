dea3aa4a9eb760405007855049af7157bae3a4d7
ragunathan467 <ragunathanm185@gmail.com>
2025-10-06 06:45:53 +0530

    commit 1

---
+++ tnsif_java_ragu1/src/AbstracticDemo/StaticDemo.java
@@ -0,0 +1,16 @@
+package AbstracticDemo;
+
+public class StaticDemo {
+	static int count;
+	static {
+		count=20;
+		System.out.println("show the static count assigned:"+count);
+	}
+	static void show() {
+		System.out.println("Method runs in static with count:"+count);
+	}
+	public static void main(String[]args) {
+		System.out.println("Static in main method:"+count);
+		show();
+	}
+}
