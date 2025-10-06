8550b8c40cb63f7d733e2fff6cd6593889e86d3d
ragunathan467 <ragunathanm185@gmail.com>
2025-10-06 07:05:56 +0530

    commit 1

---
+++ tnsif_java_ragu1/src/Package/Packagemain.java
@@ -0,0 +1,14 @@
+package com.tnsif.Package;
+
+import com.tnsif.packagesandaccessmodifiers.PackageExample;
+
+public class Packagemain {
+
+	public static void main(String[] args) {
+
+		PackageExample pe = new PackageExample();
+		pe.display();
+
+	}
+
+}
